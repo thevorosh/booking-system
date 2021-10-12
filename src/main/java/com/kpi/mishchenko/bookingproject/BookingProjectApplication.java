b8cebc67e0e4e65317162ff8cf5269db24f8aa4d
Andrii Mishchenko <mishchenko.andrii.02@gmail.com>
2021-10-12 13:22:37 +0300

    Initial commit

---
+++ src/main/java/com/kpi/mishchenko/bookingproject/BookingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kpi.mishchenko.bookingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kpi/mishchenko/bookingproject/BookingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpi.mishchenko.bookingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
